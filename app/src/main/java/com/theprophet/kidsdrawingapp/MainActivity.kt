2ab2c03a2396a73050b876686399a1e28a132e88
Michael <mjmoore87@outlook.com>
2021-12-11 19:54:36 -0500

    version 1.02

--- app/src/main/java/com/theprophet/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/theprophet/kidsdrawingapp/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
 import android.media.Image
+import android.media.MediaScannerConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -226,6 +227,7 @@ class MainActivity : AppCompatActivity() {
                                 "File saved successfully: $result",
                                 Toast.LENGTH_SHORT
                             ).show()
+                            shareImage(result)
                         }else{
                             Toast.makeText(this@MainActivity,
                                 "Something went wrong saving the file.",
@@ -298,6 +300,19 @@ class MainActivity : AppCompatActivity() {
         if(customProgressDialog != null){
             customProgressDialog?.dismiss()
             customProgressDialog = null
+        }
+    }
+
+    //this function will allow us to share image via email
+    private fun shareImage(result: String){
+        MediaScannerConnection.scanFile(this, arrayOf(result),null){
+            path, uri ->
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+            shareIntent.type = "image/png"
+            startActivity(Intent.createChooser(shareIntent, "Share"))
+
         }
     }
 
